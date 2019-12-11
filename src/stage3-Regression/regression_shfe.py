c4898486779305ab6748f3382261f5e486cd2920
cardinal2376 <cardinal2376@gmail.com>
2019-12-11 17:59:24 +0800

    update exp

--- src/stage3-Regression/regression_shfe.py
+++ src/stage3-Regression/regression_shfe.py
@@ -8,16 +8,16 @@ def main():
     hc = f["data"][1].flatten()
     spread = rb - hc
     train_len = int(len(spread) * 0.5)
-    sp_mean = spread[:train_len].mean() + 100
+    sp_mean = spread[:train_len].mean()
     sp_std = spread[:train_len].std()
-    spread_valid = spread[train_len:]
-    print("range: [{}, {}]".format(sp_mean - sp_std, sp_mean + sp_std))
-    # rb = (rb - rb.mean()) / rb.std()
-    # hc = (hc - hc.mean()) / hc.std()
+    rb_valid = rb[train_len:]
+    hc_valid = hc[train_len:]
+    spread_valid = rb_valid - hc_valid
+    print("range: [{}, {}]".format(sp_mean - 2 * sp_std, sp_mean + 2 * sp_std))
     plt.title("Plot for rb and hc")
     plt.xlabel("days")
     plt.ylabel("normed prices")
-    x = list(range(spread_valid.shape[0]))
+    x = list(range(rb_valid.shape[0]))
     # plt.plot(x, rb, label="rb")
     # plt.plot(x, hc, label="hc")
     plt.plot(x, spread_valid, label="spread")
@@ -28,31 +28,44 @@ def main():
     start_founding = 20000
     founding = start_founding
     future_cnt = 0
+    fee = 0
     for i in range(spread_valid.shape[0]):
         wb = sp_mean - 2 * sp_std
         ub = sp_mean + 2 * sp_std
         if (spread_valid[i] < wb or spread_valid[i] > ub):
-            if (spread_valid[i] < wb):
+            if (spread_valid[i] < wb and future_cnt == 0):
                 # 如果价差过低，买入rb, 卖出hc(founding - rb + hc)
                 # p_cnt = founding / abs(spread[i]) // 2
+                # print("cur valid", spread_valid[i])
                 p_cnt = start_founding * 0.3 / abs(spread_valid[i])
                 founding -= spread_valid[i] * p_cnt
-                founding -= spread_valid[i] * p_cnt * 0.00004 #手续费
-                future_cnt += 1
-            if (spread_valid[i] > ub):
+                founding -= (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004 #手续费
+                fee += (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004 #手续费
+                future_cnt += p_cnt
+            if (spread_valid[i] > ub and future_cnt == 0):
                 # 如果价差过高，买入hc, 卖出rb(founding + rb - hc)
                 # p_cnt = future_cnt // 2
+                # print("cur valid", spread_valid[i])
                 p_cnt = start_founding * 0.3 / abs(spread_valid[i])
                 founding += spread_valid[i] * p_cnt
-                founding -= spread_valid[i] * p_cnt * 0.00004  # 手续费
-                future_cnt -= 1
+                founding -= (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004 #手续费
+                fee += (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004  # 手续费
+                future_cnt -= p_cnt
         else:
             #如果回到正常区间，买入所有空头(卖出hc, 买入hb, founding + rb - hc)，卖出所有多头
-            founding += spread_valid[i] * future_cnt
-            founding -= spread_valid[i] * future_cnt * 0.00004  # 手续费
+            # print("cur valid", spread_valid[i])
+            if future_cnt > 0:
+                founding += spread_valid[i] * future_cnt
+                founding -= (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004 #手续费
+                fee += (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004  # 手续费
+            elif future_cnt < 0:
+                founding -= spread_valid[i] * abs(future_cnt)
+                founding -= (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004  # 手续费
+                fee += (rb_valid[i] + hc_valid[i]) * p_cnt * 0.00004  # 手续费
             future_cnt = 0
         print("i = {}, founding = {}, future_cnt = {}".format(i, founding, future_cnt))
-
+    print("sum of fee = ", fee)
+    print("interest ratio ", (founding / start_founding) * 100 - 100)
 
 
 if __name__ == "__main__":
